cc7d5bcd3aba68b810b5ed9a8126d2e6b5ba18ec
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2025-01-21 14:14:59 +0100

    build: implement dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import { ChatMessageListDemo } from "@/components/Demo";
+import { ModeToggle } from "@/components/theme-toggle";
 
 export default function Home() {
   return (
-    <main className="h-screen w-full max-w-3xl mx-auto p-8">
-      <ChatMessageListDemo />
-    </main>
+    <>
+      <ModeToggle />
+      <main className="h-screen w-full max-w-3xl mx-auto p-8">
+        <ChatMessageListDemo />
+      </main>
+    </>
   );
 }
---
+++ components/theme-toggle.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+export function ModeToggle() {
+  const { setTheme, theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <button
+      className="absolute top-3 right-3 inline-flex items-center justify-center gap-2 rounded-md border border-input bg-transparent px-2 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground"
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
+      {theme === "dark" ? (
+        <Sun className="h-4 w-4" />
+      ) : (
+        <Moon className="h-4 w-4" />
+      )}
+    </button>
+  );
+}
